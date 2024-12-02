b2340a77645a2e56192ad326102abadfcbdeb0b9
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2024-12-02 14:22:16 -0300

    firebaseConnection atualizado

---
+++ projetos/src/firebaseConnection.jsx
@@ -0,0 +1,17 @@
+import {initializeApp} from "firebase/App"
+import {getFirestore} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYJ6wDiTJTQnN7uVJCJhW7fc2v4vye53g",
+  authDomain: "projeto-rafa-7af5c.firebaseapp.com",
+  projectId: "projeto-rafa-7af5c",
+  storageBucket: "projeto-rafa-7af5c.firebasestorage.app",
+  messagingSenderId: "67285228023",
+  appId: "1:67285228023:web:ff945c7daebe21b4ab36f2",
+  measurementId: "G-HGWP7GGWYY"
+};
+
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getFirestore(firebaseApp)
+
+export {db}
