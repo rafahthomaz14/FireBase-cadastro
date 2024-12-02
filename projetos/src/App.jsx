28b02c3656e614cd694bbe001d9c356a19e17ea9
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2024-12-02 15:38:43 -0300

    novo codigo

--- projetos/src/App.jsx
+++ projetos/src/App.jsx
@@ -1,24 +1,51 @@
-import './App.css'
-import { db } from './firebaseConnection'
-import { doc, collection, addDoc, updateDoc, deleteDoc, onSnapshot } from 'firebase/firestore'
 import React, { useState, useEffect } from 'react'
-
+import { db } from './FirebaseConnection'
+import { doc, setDoc, collection, addDoc, getDoc, onSnapshot, updateDoc, deleteDoc } from 'firebase/firestore'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
 function App() {
   const [nome, setNome] = useState('')
   const [idade, setIdade] = useState('')
   const [telefone, setTelefone] = useState('')
-  const [lista, setListas] = useState([])
+  const [listas, setListas] = useState([])
+  const [editCliente, setEditCliente] = useState('')
 
-  //adicionar cliente 
-  async function Cadastro() {
-    const docRef = addDoc(collection(db, 'cliente'), {
+  //Listar clientes 
+  useEffect(() => {
+    async function ListarClientes() {
+      const docRef = onSnapshot(collection(db, 'cliente'), (snapshot) => {
+        let todasLista = []
+
+        snapshot.forEach((doc) => {
+          todasLista.push({
+            id: doc.id,
+            nome: doc.data().nome,
+            idade: doc.data().idade,
+            telefone: doc.data().telefone,
+          })
+        })
+
+        setListas(todasLista)
+      })
+
+    }
+    ListarClientes()
+  }, [])
+
+
+
+  // Cadastrar clientes
+  async function cadastro() {
+    await addDoc(collection(db, 'cliente'), {
       nome: nome,
       idade: idade,
       telefone: telefone,
 
     })
       .then(() => {
-        alert('Cliente adicionado!')
+        toast.success('Cliente cadastrado!')
         setNome('')
         setIdade('')
         setTelefone('')
@@ -28,62 +55,95 @@ function App() {
       })
   }
 
-
-  //Listar clientes 
-  useEffect(() => {
-    //cria a function
-    async function ListarClientes() {
-      //define a nova variavel
-      let NovaLista = []
-      // chamar onSnapshot passando o snapshot
-      const docRef = onSnapshot(collection(db, 'cliente'), (snapshot) => {
-        //percorrer a lista usando o forEach
-        snapshot.forEach((doc) => {
-          //adicionando os itens a nova variavel com o push 
-          NovaLista.push({
-            id: doc.id,
-            nome: doc.data().nome,
-            idade: doc.data().idade,
-            telefone: doc.data().telefone,
-
-          })
-        })
-        setListas(NovaLista)
+  //Editar Clientes
+  async function Edit() {
+    const docRef = doc(db, 'cliente', editCliente)
+    await updateDoc(docRef, {
+      nome: nome,
+      idade: idade,
+      telefone: telefone,
+    })
+      .then(() => {
+        toast.success('Cliente atualizado !')
+        setEditCliente('')
+        setNome('')
+        setIdade('')
+        setTelefone('')
       })
-    }
-    ListarClientes()
-  }, [])
+      .catch((error) => {
+        toast.danger('Erro' + error)
+      })
+  }
 
+  //Remover Clientes
+  async function Remover(id) {
+    const docRef = doc(db, 'cliente', id)
+    await deleteDoc(docRef)
+      .then(() => {
+        toast.success('Cliente Removido!')
+      })
+      .catch((error) => {
+        toast.danger('Erro' + error)
+      })
+  }
 
 
   return (
-    <>
-      <h1>Formulário de Cadastro</h1>
+    <div className='container'>
+      <ToastContainer autoClose={3000} />
+      <div className="row">
+        <div className="form col-lg-4  col-sm-12">
+          <h3>Formulário</h3>
 
-      <h4>Nome :</h4>
-      <input type="text" placeholder='Digite o nome...' value={nome} onChange={(e) => setNome(e.target.value)} />
+          <h5>Id do Cliente : </h5>
+          <input type="text" placeholder='Selecione o ID para Editar ...' value={editCliente} onChange={(e) => setEditCliente(e.target.value)} />
 
-      <h4>Idade :</h4>
-      <input type="text" placeholder='Digite a idade...' value={idade} onChange={(e) => setIdade(e.target.value)} />
+          <h5>Nome :</h5>
+          <input type="text" placeholder='Digite o Nome ...' value={nome} onChange={(e) => setNome(e.target.value)} />
 
-      <h4>Telefone :</h4>
-      <input type="text" placeholder='Digite o telefone...' value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+          <h5>Idade :</h5>
+          <input type="text" placeholder='Digite ...' value={idade} onChange={(e) => setIdade(e.target.value)} />
 
-      <button onClick={Cadastro}>Cadastrar</button>
-      <hr />
-      <h1>Lista dos Clientes Cadastrado </h1>
-      {lista.map((item) => {
-        return (
-          <div>
-            <h4>{item.id}</h4>
-            <h4>{item.nome}</h4>
-            <h4>{item.idade}</h4>
-            <h4>{item.telefone}</h4>
+          <h5>Telefone :</h5>
+          <input type="text" placeholder='Digite o Telefone ...' value={telefone} onChange={(e) => setTelefone(e.target.value)} />
+          <button className='botao mt-3' onClick={cadastro}>Cadastrar</button>
+          <button className='botao' onClick={Edit}>Editar</button>
+        </div>
+        <div className="col-lg-8  col-sm-12">
+          <h3>Lista de Clientes</h3>
+          <div className="table-container">
+            <table className="table table-striped table-hover">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nome</th>
+                  <th>Idade</th>
+                  <th>Telefone</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                {listas.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id}</td>
+                    <td>{item.nome}</td>
+                    <td>{item.idade} anos</td>
+                    <td>{item.telefone}</td>
+                    <td>
+                      <button className="btn btn-danger" onClick={() => Remover(item.id)}>
+                        Remover
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
-        )
-      })}
-    </>
+        </div>
+      </div>
+    </div>
   )
 }
+
 
 export default App
